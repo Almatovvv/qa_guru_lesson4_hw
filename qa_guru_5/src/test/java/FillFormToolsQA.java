66ed6bc9fc22bee93553069f91cd6b3ab1f06cb6
Almatov Chingiz <QAChingiz@gmail.com>
2021-03-21 20:32:28 +0600

    updated according to feedback

--- qa_guru_5/src/test/java/FillFormToolsQA.java
+++ qa_guru_5/src/test/java/FillFormToolsQA.java
@@ -12,26 +12,47 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FillFormToolsQA {
 
+    String firstName = "Автотест";
+    String lastName = "Селенидов";
+    String userEmail = "Stable@autotests.com";
+    String userNumber = "1234567890";
+    String monthSelect = "June";
+    String yearSelect = "1969";
+    String subjectsInput = "Maths";
+    String currentAddress = "Kazakhstan, Almaty, Autotester str.";
+    String userState = "Haryana";
+    String userCity = "Karnal";
+
     @Test
     void selenideSearchTest() {
         open("https://demoqa.com/automation-practice-form");
 
-        $(By.xpath("//input[@id='firstName']")).waitUntil(visible, 20000).setValue("Автотест");
-        $(By.xpath("//input[@id='lastName']")).shouldBe(visible).setValue("Селенидов");
-        $(By.xpath("//input[@id='userEmail']")).shouldBe(visible).setValue("Stable@autotests.com");
-        $(By.xpath("//label[contains(text(),'Other')]")).shouldBe(visible).click();
-        $(By.xpath("//input[@id='userNumber']")).shouldBe(visible).setValue("1234567890");
-        $(By.xpath("//input[@id='dateOfBirthInput']")).click();
-        $(By.xpath("//select[@class='react-datepicker__month-select']")).selectOption("June");
-        $(By.xpath("//select[@class='react-datepicker__year-select']")).selectOption("1969");
-        $(By.xpath("//div[@aria-label='Choose Tuesday, June 10th, 1969']")).click();
-        $(By.xpath("//input[@id='subjectsInput']")).setValue("math").pressEnter();
-        $(By.xpath("//label[contains(text(),'Sports')]")).click();
-        $(By.xpath("//input[@id='uploadPicture']")).scrollTo().uploadFile(new File("src/test/files/Test.jpg"));
-        $(By.xpath("//textarea[@id='currentAddress']")).setValue("Kazakhstan, Almaty, Autotester str.");
-        $(By.xpath("//input[@id='react-select-3-input']")).setValue("Haryana").pressEnter();
-        $(By.xpath("//input[@id='react-select-4-input']")).shouldBe(enabled).setValue("Karnal").pressEnter();
-        $(By.xpath("//button[@id='submit']")).click();
-        $(By.xpath("//div[@class=\"table-responsive\"]")).shouldHave(text("Автотест"), text("Селенидов"), text("Stable@autotests.com"));
+        $("#firstName").setValue(firstName);
+        $("#lastName").shouldBe(visible).setValue(lastName);
+        $("#userEmail").shouldBe(visible).setValue(userEmail);
+        $x("//label[contains(text(),'Other')]").shouldBe(visible).click();
+        $("#userNumber").shouldBe(visible).setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $x("//select[@class='react-datepicker__month-select']").selectOption(monthSelect);
+        $x("//select[@class='react-datepicker__year-select']").selectOption(yearSelect);
+        $x("//div[@aria-label='Choose Tuesday, June 10th, 1969']").click();
+        $("#subjectsInput").setValue(subjectsInput).pressEnter();
+        $x("//label[contains(text(),'Sports')]").click();
+        $("#uploadPicture").scrollTo().uploadFile(new File("src/test/resources/Test.jpg"));
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(userState).pressEnter();
+        $("#react-select-4-input").setValue(userCity).pressEnter();
+        $("#submit").click();
+        $(By.xpath("//div[@class=\"table-responsive\"]")).shouldHave(
+                text(firstName + " " +lastName),
+                text(userEmail),
+                text("Other"),
+                text(userNumber),
+                text("10 June,1969"),
+                text(subjectsInput),
+                text("Sports"),
+                text("Test.jpg"),
+                text(currentAddress),
+                text(userState + " " + userCity));
     }
 }
