c9bec2ecd3243f56e62a8ee2f0b9d5e4a89cfae3
Almatov Chingiz <QAChingiz@gmail.com>
2021-03-24 19:40:31 +0600

    hw for qa guru lesson3

---
+++ qa_guru_lesson3_hw/src/test/java/GitHubProjectWiki.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import javax.xml.xpath.XPath;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubProjectWiki {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = "1366x768";
+    }
+
+    @Test
+    void githubSearchTest() {
+        open("https://github.com/");
+
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[href='/selenide/selenide']").click();
+        $("[href='/selenide/selenide/wiki']").click();
+        $("[data-filterable-for='wiki-pages-filter']").$("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
